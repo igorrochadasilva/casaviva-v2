30c5daa56833a770c169d61d51891b854a9017d0
igorsilva <igor.silva@performait.com>
2021-10-26 21:05:14 -0300

    created simulation html and css

--- start/routes.ts
+++ start/routes.ts
@@ -21,7 +21,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 // Other Routes...
-Route.get('/', async ({ view }: HttpContextCosntract) => {
+Route.get('/', async ({ view }: HttpContextContract) => {
   return view.render('index')
 }).as('not_found')
 
