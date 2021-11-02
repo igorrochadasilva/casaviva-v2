5d3000c8a11fba1588ffad117f192d5f1e7d59d0
igorsilva <igor.silva@performait.com>
2021-11-01 21:23:38 -0300

    feature/cookie-alert

--- resources/client/components/CookiesAlert/index.jsx
+++ resources/client/components/CookiesAlert/index.jsx
@@ -1,29 +1,44 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-import '../../../assets/scss/components/CookiesAlert/index.scss';
+import '../../../assets/scss/components/CookiesAlert/index.scss'
 
 const CookiesAlert = () => {
-    return (
-        <div id="cookiesAlert" class="show" data-v-d81c723e="">
-            <div class="el-row is-justify-center is-align-middle el-row--flex">
-                <div class="el-col el-col-24 el-col-xs-24 el-col-sm-18">
-                    <p>
-                        Usamos cookies para ajudar a personalizar conteúdo, adaptar e medir anúncios e oferecer uma experiência mais segura. Ao navegar no site, você concorda com o uso de cookies para coletar informações.
-                        <a href="/politica-de-cookies" class="">
-                            Leia nossa Política de cookies.
-                        </a>
-                    </p>
-                </div>
-                <div class="el-col el-col-24 el-col-xs-24 el-col-sm-6">
-                    <div class="wrapButtons">
-                        <a class="btn-primary-red">
-                            Eu aceito
-                        </a>
-                    </div>
-                </div>
-            </div>
+  const cookieStatus = localStorage.getItem('acceptCookie')
+  const modal = useRef(null)
+
+  const handleClick = () => {
+    const { current } = modal
+
+    localStorage.setItem('acceptCookie', 'true')
+    current.classList.remove('show')
+    current.classList.add('hide')
+  }
+
+  return !cookieStatus ? (
+    <div id="cookiesAlert" className="show" ref={modal}>
+      <div className="el-row is-justify-center is-align-middle el-row--flex">
+        <div className="el-col el-col-24 el-col-xs-24 el-col-sm-18">
+          <p>
+            Usamos cookies para ajudar a personalizar conteúdo, adaptar e medir anúncios e oferecer
+            uma experiência mais segura. Ao navegar no site, você concorda com o uso de cookies para
+            coletar informações.
+            <a href="/politica-de-cookies" className="">
+              Leia nossa Política de cookies.
+            </a>
+          </p>
         </div>
-    )
+        <div className="el-col el-col-24 el-col-xs-24 el-col-sm-6">
+          <div className="wrapButtons">
+            <a className="btn-primary-red" onClick={() => handleClick()}>
+              Eu aceito
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    false
+  )
 }
 
 export default CookiesAlert
