fcc57afc0fef0fd226ba2612190591bbc21958b1
yuri gama <yurigama_profissional@hotmail.com>
2021-11-06 12:24:41 -0300

    fix boas praticas react

--- resources/client/components/BannerInstitucional/index.jsx
+++ resources/client/components/BannerInstitucional/index.jsx
@@ -3,20 +3,20 @@ import '../../../assets/scss/components/BannerInstitucional/index.scss';
 
 const BannerInstitucional = () => {
     return (
-        <div data-v-7c86ee3e="" data-v-fd5dd3e0="" data-v-e4caeaf8="" class="BannerInstitucional">
-            <picture data-v-7c86ee3e="">
-                <source media="(max-width: 767px)" srcset="https://casaviva-prod.s3.amazonaws.com/public/current-build/img/874950a.jpg" data-v-354ad60e="" />
-                <img src="https://i.imgur.com/XIxQ7TM.png" alt="Banner institucional CasaViva" class="img-fluid" data-v-354ad60e="" />
+        <div className="BannerInstitucional">
+            <picture >
+                <source media="(max-width: 767px)" srcSet="https://casaviva-prod.s3.amazonaws.com/public/current-build/img/874950a.jpg" />
+                <img src="https://i.imgur.com/XIxQ7TM.png" alt="Banner institucional CasaViva" className="img-fluid" />
             </picture>
-            <div class="wrap-container" data-v-354ad60e="">
-                <div class="container_app" data-v-354ad60e="">
-                    <div class="titleWrapper" data-v-354ad60e="">
-                        <h1 data-v-354ad60e="">
+            <div className="wrap-container">
+                <div className="container_app">
+                    <div className="titleWrapper">
+                        <h1>
                             O sonho da sua casa própria começa aqui
                         </h1>
                     </div>
-                    <div class="action" data-v-354ad60e="">
-                        <a href="/minha-casa-minha-vida" class="btn btn-primary-white" data-v-354ad60e="">
+                    <div className="action">
+                        <a href="/minha-casa-minha-vida" className="btn btn-primary-white">
                             Saiba mais
                         </a>
                     </div>
